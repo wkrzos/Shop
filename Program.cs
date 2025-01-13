9565463f7cbafddac6c812ead03ffc0cf6c87bf2
wkrzos <w.krzos.contact@gmail.com>
2025-01-13 02:36:36 +0100

    feat: change sql server to sqlite

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlite("Data Source=app.db"));
 
 var app = builder.Build();
 
